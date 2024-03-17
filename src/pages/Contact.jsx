f8699455dec7856a159bf8b94303cb4a5b4798c0
Lyse A. Aneze <64477195+A-L-A@users.noreply.github.com>
2024-03-17 22:49:31 +0400

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import ContactForm from "../components/ContactForm";
 function Contact () {
     return (
         <Box p="12" minH="100vh">
-            <Heading size="2xl" mb="8">Contact Us:</Heading>
+            <Heading size="2xl" mb="8">Contact Us</Heading>
         <ContactForm />
         </Box>
     )
